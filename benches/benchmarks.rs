0b6dc8640a53056f4155093548e114f12c167c4c
Simisticot <simi.pra@gmail.com>
2025-03-10 15:20:49 +0100

    Add perft benchmarking

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,14 +1,20 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use libchessticot::{Planner, Player, Position};
-fn minimax_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("minimax");
+fn planner_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("planner move search");
     group.sample_size(10);
     let position =
         Position::from_fen("r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 0 1");
-    group.bench_function("minimax 3", |b| {
+    group.bench_function("offer planner move", |b| {
         b.iter(|| Planner {}.offer_move(black_box(&position)))
     });
 }
 
-criterion_group!(benches, minimax_benchmark);
+fn perft_3_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("perft 3");
+    let position = Position::initial();
+    group.bench_function("perft 3", |b| b.iter(|| position.perft(3)));
+}
+
+criterion_group!(benches, planner_benchmark, perft_3_benchmark);
 criterion_main!(benches);
